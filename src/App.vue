2dd09ebcc9356a5b2f500799ff0fe7d2814e377a
bekirbostanci <bekirbostanci@gmail.com>
2025-01-20 00:48:41 +0100

    feat: add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Validator from './views/Validator.vue';
 import Toaster from '@/components/ui/toast/Toaster.vue';
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <Analytics />
   <Toaster />
   <Validator></Validator>
 </template>
